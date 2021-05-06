a692785fc95d81522220444f220efc99686a174a
fengdong <fengdong1992@gmail.com>
2021-05-06 21:24:24 +0800

    update exchange.sol
    update exchange.sol

---
+++ contracts/exchange_contract/OwnableDelegateProxy.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <0.8.0;
+
+contract OwnableDelegateProxy is OwnedUpgradeabilityProxy {
+
+    constructor(address owner, address initialImplementation, bytes calldata)
+        public
+    {
+        setUpgradeabilityOwner(owner);
+        _upgradeTo(initialImplementation);
+        require(initialImplementation.delegatecall(calldata));
+    }
+
+}
