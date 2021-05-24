5f4872c05632b1c19b00477a79b6b98f5dbcebd1
Wildeagle <43267322+WildEagleDon@users.noreply.github.com>
2021-05-24 21:48:45 +0800

    Delete exchange_contract directory

--- exchange_contract/contracts/ERC20Basic.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.6.0 <0.8.0;
-
-abstract contract ERC20Basic {
-  function totalSupply() public view returns (uint256);
-  function balanceOf(address who) public view returns (uint256);
-  function transfer(address to, uint256 value) public returns (bool);
-  event Transfer(address indexed from, address indexed to, uint256 value);
-}
